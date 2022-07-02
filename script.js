87a76a026ee353bd2b76746dd97d1b3026cae0e8
ascodeasice <ab0988956087@gmail.com>
2022-07-02 15:52:24 +0800

    Finish switching units. User can switch between Celcius and Fahrenheit.

--- script.js
+++ script.js
@@ -1,33 +1,64 @@
 //NOTE this API key is exposed, but it's fine because this one is free
 const apiKey = "accb9d3c49626138ea02283155a14f92";
+const cityName = document.getElementById("cityName");
+const description = document.getElementById("description");
+const feelsLike = document.getElementById("feelsLike");
+const humidity = document.getElementById("humidity");
+const temp = document.getElementById("temp");
+const cloudiness = document.getElementById("cloudiness");
+const search = document.getElementById("search");
+const tempUnitsBtn = document.getElementById("tempUnitsBtn");
+let tempUnits = "°C";
+let data;//will be initailized later
+
+//TODO css
+//TODO fonts
 
 function celToFah(val) {
-  return Number(val) * 1.8 + 32;
+  return Math.round(Number(val) * 1.8 + 32);
 }
 
 function fahToCel(val) {
-  return (Number(val) - 32) / 1.8
+  return Math.round((Number(val) - 32) / 1.8);
+}
+
+function capitalize(str) {
+  //capitalize every word
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i == 0 || str[i - 1] == " ") {
+      result += str[i].toUpperCase();
+    }
+    else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 function displayData(data) {
-  console.table(data);
+  cityName.innerText = "Location:" + data.cityName;
+  description.innerText = capitalize(data.description);
+  humidity.innerText = "Humidity:" + data.humidity;
+  cloudiness.innerText = "Cloudiness:" + data.cloudiness;
+  feelsLike.innerText = "Feels Like:" + (tempUnits == "°F" ? celToFah(data.feelsLike) : data.feelsLike) + tempUnits;
+  temp.innerText = "Temperature:" + (tempUnits == "°F" ? celToFah(data.temp) : data.temp) + tempUnits;
 }
 
 function processJSON(data) {
-  console.log(data);
   return {
-    descripton: data.weather[0].description,
-    feelsLike: Math.round(data.main.feels_like),
-    humidity: data.main.humidity,
-    temp: Math.round(data.main.temp),//Celcius
     cityName: data.name,
+    description: data.weather[0].description,
+    feelsLike: String(Math.round(data.main.feels_like)),
+    humidity: String(data.main.humidity) + "%",
+    temp: String(Math.round(data.main.temp)),
     cloudiness: String(data.clouds.all) + "%"
   }
 }
 
 async function getWeatherData(location) {
+  //NOTE Data is in Celcius
   try {
-    //NOTE get Celcius
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`, {
       mode: 'cors'
     });
@@ -35,9 +66,26 @@ async function getWeatherData(location) {
     data = processJSON(json);
     displayData(data);
   }
-  catch (error) {
+  catch (error) {//TODO display error when location isn't found
     console.log(error);
   }
 }
+
+search.addEventListener("keyup", function (event) {
+  if (event.code === 'Enter') {
+    //when enter is pressed
+    getWeatherData(search.value);
+  }
+});
+
+tempUnitsBtn.addEventListener("click", () => {
+  if (tempUnits == "°C") {
+    tempUnits = "°F";
+  }
+  else {
+    tempUnits = "°C";
+  }
+  displayData(data);
+})
 
 getWeatherData("Tainan");//default
